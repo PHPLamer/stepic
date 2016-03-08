76185a5063d6971660a4d06489f304a599b91490
Spicyn Sergey <phplamer@mail.ru>
2016-03-09 00:01:02 +0300

    Bug fix.

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    body = env.get('QUERY_STRING').replace('&', '<br>')
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    body = env.get('QUERY_STRING').replace('&', '\r\n')
     return [body]
