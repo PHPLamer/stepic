264452372fd897cc7c8bbe1fc49f789d8d370597
Spicyn Sergey <phplamer@mail.ru>
2016-03-13 00:21:42 +0300

    Restart MySQL.

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ sudo ln -s /home/box/web/etc/gunicorn_django.conf   /etc/gunicorn.d/django.conf
 sudo /etc/init.d/gunicorn restart
 
 #MySQL
+sudo service mysql restart
 mysql -uroot -e "CREATE DATABASE stepic_test;"
 mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_test.* TO 'box'@'localhost' WITH GRANT OPTION;"
